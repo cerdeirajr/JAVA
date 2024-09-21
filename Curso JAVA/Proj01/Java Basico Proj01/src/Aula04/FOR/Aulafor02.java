3181b85e68dcbd72c7570d5b5716b5782e49fc0a
cerdeirajr <cerdeirajr@gmail.com>
2024-09-21 19:16:23 -0300

    Aula04 FOR 2 exercicios

---
+++ Curso JAVA/Proj01/Java Basico Proj01/src/Aula04/FOR/Aulafor02.java
@@ -0,0 +1,15 @@
+package Aula04.FOR;
+
+public class Aulafor02 {
+    public static void main(String[] args) {
+
+        for (int i=1; i>=-5; i-- ){
+            System.out.println(i);
+        }
+
+    }
+}
+
+
+//for (inicialização; condição; incremento/decremento) {
+//    // Bloco de código a ser repetido
---
+++ Curso JAVA/Proj01/Java Basico Proj01/src/Aula04/FOR/aulafor01.java
@@ -0,0 +1,19 @@
+package Aula04.FOR;
+
+public class aulafor01 {
+    public static void main(String[] args) {
+
+for (int i=1; i<=10; i++ ){
+    System.out.println(i);
+}
+
+
+    }
+}
+
+
+
+// Imprima um numero de 1 a 10.
+//for (inicialização; condição; incremento/decremento) {
+//    // Bloco de código a ser repetido
+//}
