af8fefe5daa88a0079147763b7ca70690fab23d9
cerdeirajr <cerdeirajr@gmail.com>
2024-10-02 16:18:09 -0300

    Aula5 Array

--- Curso JAVA/Proj01/Java Basico Proj01/src/Aula05/aulaArray01.java
+++ Curso JAVA/Proj01/Java Basico Proj01/src/Aula05/aulaArray01.java
@@ -1,2 +1,19 @@
-package Aula05;public class aulaArray01 {
+//Explicação de Arrays em Java
+//Um array em Java é uma estrutura de dados que armazena múltiplos valores de um mesmo tipo em uma única variável. Um array tem tamanho fixo e armazena elementos de forma contínua na memória.
+//
+//Declaração de um Array
+//Declaração e Inicialização:
+
+package Aula05;
+
+public class aulaArray01 {
+
+    public static void main(String[] args) {
+        int[] numeros = {10,20,30,40,50};
+
+        for (int i = 0; i< numeros.length; i++) {
+            System.out.println("Elemento no indice " +i + ":" + numeros[i]);
+        }
+    }
+
 }
